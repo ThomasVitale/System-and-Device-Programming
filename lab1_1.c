04014c8fbe27ba5a52435a99af31c84c0e479b86
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2016-03-14 15:06:42 +0100

    minor update

--- lab1_1.c
+++ lab1_1.c
@@ -38,7 +38,7 @@ int main(int argc, char** argv) {
 	for (i=0; i<n1; i++) {
 		v1[i] = randomNumber(10, 100);
 	}
-	fprintf(stdout, "%s", "Vector v1: ");
+	fprintf(stdout, "Vector v1: ");
 	printVector(v1, n1);
 	
 	// Fills v2 with n2 random numbers between 20-100
@@ -46,7 +46,7 @@ int main(int argc, char** argv) {
 	for (i=0; i<n2; i++) {
 		v2[i] = randomNumber(20, 100);
 	}
-	fprintf(stdout, "%s", "Vector v2: ");
+	fprintf(stdout, "Vector v2: ");
 	printVector(v2, n2);
 	
 	// Sort v1 and v2 (increasing values)
@@ -54,32 +54,32 @@ int main(int argc, char** argv) {
 	sortVector(v1, n1);
 	sortVector(v2, n2);
 	
-	fprintf(stdout, "%s", "Vector v1 (sorted): ");
+	fprintf(stdout, "Vector v1 (sorted): ");
 	printVector(v1, n1);
-	fprintf(stdout, "%s", "Vector v2 (sorted): ");
+	fprintf(stdout, "Vector v2 (sorted): ");
 	printVector(v2, n2);
 
 	// Save the content of vectors v1 and v2 in two text files fv1.txt and fv2.txt, respectively
 
 	if ((fv1 = fopen("fv1.txt", "w+")) == NULL) {
-		fprintf(stderr, "%s", "Error opening fv1.txt\n");
+		fprintf(stderr, "Error opening fv1.txt\n");
 		return -2;
 	}
 
 	if ((fv2 = fopen("fv2.txt", "w+")) == NULL) {
-		fprintf(stderr, "%s", "Error opening fv2.txt\n");
+		fprintf(stderr, "Error opening fv2.txt\n");
 		return -3;
 	}
 
 	for (i=0; i<n1; i++) {
 		fprintf(fv1, "%d ", v1[i]);
 	}
-	fprintf(stdout, "%s", "File fv1.txt scritto con successo \n");
+	fprintf(stdout, "File fv1.txt scritto con successo \n");
 	
 	for (i=0; i<n2; i++) {
 		fprintf(fv2, "%d ", v2[i]);
 	}
-	fprintf(stdout, "%s", "File fv2.txt scritto con successo \n");
+	fprintf(stdout, "File fv2.txt scritto con successo \n");
 	
 	fclose(fv1);
 	fclose(fv2);
@@ -87,26 +87,26 @@ int main(int argc, char** argv) {
 	// Save the content of vectors v1 and v2 in two binary files fv1.b and fv2.b, respectively
 	
 	if ((fv1b = fopen("fv1.b", "w+")) == NULL) {
-		fprintf(stderr, "%s", "Error opening fv1.b\n");
+		fprintf(stderr, "Error opening fv1.b\n");
 		return -4;
 	}
 
 	if ((fv2b = fopen("fv2.b", "w+")) == NULL) {
-		fprintf(stderr, "%s", "Error opening fv2.b\n");
+		fprintf(stderr, "Error opening fv2.b\n");
 		return -5;
 	}
 	
 	if (fwrite(v1, sizeof(int), n1, fv1b) != n1) {
-		fprintf(stderr, "%s", "Error writing fv1.b");
+		fprintf(stderr, "Error writing fv1.b");
 		return -6;
 	}
-	fprintf(stdout, "%s", "File fv1.b scritto con successo \n");
+	fprintf(stdout, "File fv1.b scritto con successo \n");
 	
 	if (fwrite(v2, sizeof(int), n2, fv2b) != n2) {
-		fprintf(stderr, "%s", "Error writing fv2.b");
+		fprintf(stderr, "Error writing fv2.b");
 		return -7;
 	}
-	fprintf(stdout, "%s", "File fv2.b scritto con successo \n");
+	fprintf(stdout, "File fv2.b scritto con successo \n");
 	
 	fclose(fv1b);
 	fclose(fv2b);

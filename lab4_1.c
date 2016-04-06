afc95e14568ca2f98ef27087040de8e732dedc46
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2016-04-06 18:38:35 +0200

    Added files via upload

---
+++ lab4_1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char** argv) {
+
+	FILE* fb_out;
+	int i, n, x;
+
+	/* Check number of arguments */
+	if (argc != 3) {
+		fprintf(stderr, "Syntax: %s n fileOutput\n", argv[0]);
+		return -1;
+	}
+	
+	n = atoi(argv[1]); // total number to be generated
+	
+	if ((fb_out = fopen(argv[2], "wb")) == NULL) {
+		fprintf(stderr, "Error opening the output file.\n");
+		return -2;
+	} 
+	
+	srand(time(NULL)); // seed rand() function
+	
+	/* Generate n random integers */
+	for (i=0; i<n; i++) {
+		x = rand();
+		fwrite(&x, sizeof(int), 1, fb_out);
+		fprintf(stdout, "%d ", x);
+	}
+	fprintf(stdout, "\n");
+	
+	fclose(fb_out);
+	
+	return 0;
+}
